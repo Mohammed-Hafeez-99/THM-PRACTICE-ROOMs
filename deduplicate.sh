9b8c7b40aef618070a2ee838742bd3de2ef4bbd4
Mohammed-Hafeez-99 <sskzm805@gmail.com>
2025-11-02 08:26:12 +0530

    spacing

--- deduplicate.sh
+++ deduplicate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-awk '!seen[$0]++' source.md > README.md
-
+#awk '!seen[$0]++' source.md > README.md
+awk '{ key=$0; gsub(/ /,"",key); if (!seen[key]++) print $0 }' source.md > README.md
 sed -i '3a\   
 ' README.md
 
